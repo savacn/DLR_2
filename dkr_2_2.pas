9455abf32eb3c599ea4bb99f3e1f4e2c028d547b
Savely <112480298+savacn@users.noreply.github.com>
2023-12-12 23:17:46 +0300

    Add files via upload

---
+++ dkr_2_2.pas
@@ -0,0 +1,34 @@
+﻿program dkr_2_2;
+
+var
+  source, sub: string;
+  i, j: integer;
+  iss: boolean;
+
+begin
+  writeln('Введите исходную строку: ');
+  readln(source);
+  
+  writeln('Введите подстроку: ');
+  readln(sub);
+  
+  i := 1;
+  j := 1;
+  iss := false;
+  
+  while (i <= length(source)) and (j <= length(sub)) do
+  begin
+    if source[i] = sub[j] then
+    begin
+      j := j + 1;
+      if j > length(sub) then
+        iss := true;
+    end;
+    i := i + 1;
+  end;
+  
+  if iss then
+    writeln('Подстрока является подпоследовательностью исходной строки')
+  else
+    writeln('Подстрока не является подпоследовательностью исходной строки');
+end.
